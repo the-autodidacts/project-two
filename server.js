559ef370c009f4d0e93d00b60078b964f98a08f8
Yoan Ante <yoanante@gmail.com>
2015-10-09 10:30:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    PORT    = process.env.PORT || 5432,
+    server  = express();
+
+server.get('/test', function (req, res){
+  res.write("Welcome to my app");
+  res.end();
+});
+
+server.listen(PORT, function(){
+  console.log("SERVER IS UP ON PORT:", PORT)
+});
+    
