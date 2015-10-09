6b46e9f24ae8700d76a2fe055b8d46bec0fdcba6
Yoan Ante <yoanante@gmail.com>
2015-10-09 11:09:15 -0400

    fixed typos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     PORT    = process.env.PORT || 5432,
     server  = express(),
-    MONGOURI= process.enf.MONGOLAB_URI,
+    MONGOURI= process.env.MONGOLAB_URI || "mongodb://localhost:27017",
     dbname  = 'project2'
     mongoose= require('mongoose');
 
