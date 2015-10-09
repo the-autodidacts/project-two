03e43cc33ac943d0879891822bfafe520388973e
Yoan Ante <yoanante@gmail.com>
2015-10-09 11:40:45 -0400

    mongouri logger

--- server.js
+++ server.js
@@ -2,16 +2,32 @@ var express = require('express'),
     PORT    = process.env.PORT || 5432,
     server  = express(),
     MONGOURI= process.env.MONGOLAB_URI || "mongodb://localhost:27017",
-    dbname  = 'project2'
+    DBNAME  = 'project2'
     mongoose= require('mongoose');
 
+console.log("mongouri is", MONGOURI);
 
 server.get('/test', function (req, res){
   res.write("Welcome to my app");
   res.end();
 });
 
-mongoose.connect(MONGOURI+ dbname);
+mongoose.connect(MONGOURI + "/" + DBNAME);
 server.listen(PORT, function (){
-  console.log("Hey");
+  console.log("Hey listening on PORT: ", PORT);
 });
+
+// var express = require('express'),
+//   PORT = process.env.PORT || 6667,
+//   server = express(),
+//   MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhost:27017",
+//   dbname = "project_two_db"
+//   mongoose = require('mongoose');
+//   server.get('/test', function(req, res){
+//     res.write("fuck off");
+//     res.end();
+//   });
+// mongoose.connect(MONGOURI + "/" + dbname)
+//   server.listen(PORT,function(){
+//     console.log("SERVER IS UP ON PORT:", PORT);
+//   })
