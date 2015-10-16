1b6cfe8296eca052f358238a2bdd3e0e25085904
Yoan Ante <yoanante@gmail.com>
2015-10-15 20:56:22 -0400

    committing to push to heroku

--- controllers/articles.js
+++ controllers/articles.js
@@ -34,14 +34,6 @@ router.get('/latest', function (req, res){
   }
 });
 
-router.get('/:id/show', function (req, res) {
-  if (!res.locals.currentUser.email){
-    res.redirect(302, '/');
-  } else {
-      res.render('article/show')
-  }
-});
-
 router.get('/new', function (req, res) {
   if (!res.locals.currentUser.email){
     res.redirect(302, '/');
@@ -88,6 +80,38 @@ router.get('/:id/edit', function (req, res) {
   });
 });
 
+
+router.get('/:id', function (req, res) {
+  var articleID = req.params.id;
+
+  Article.findOne({
+    _id: articleID
+  }, function (err, foundArticle){
+    if (err){
+      res.write("Article ID is bad")
+      res.end();
+    } else {
+      res.render('articles/show', {
+        article: foundArticle
+      });
+    }
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  var articleID = req.params.id;
+  // alert("You are about to delete this article. Unfortunately event though this window is here there is nothing you can do. If you changed your mind.");
+  Article.remove({
+    _id: articleID
+  }, function (err) {
+    if (err){
+      res.write("Could not delete article")
+    }
+    else{
+      res.redirect(302, 'latest')
+    }
+  })
+});
 router.patch('/:id', function (req, res) {
   var articleID                = req.params.id,
   articleParams                = req.body.article;  articleParams.lastEdit       = dateString,
